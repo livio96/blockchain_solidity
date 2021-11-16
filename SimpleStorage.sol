85ae14c5222267f56b9a9a900a9a79fac1af5cd0
Livio Beqiri <liviob@live.com>
2021-11-15 21:31:50 -0500

    Update SimpleStorage.sol

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,33 +2,29 @@
 
 pragma solidity >=0.6.0 <0.9.0;
 
-
 contract SimpleStorage {
+
+    uint256 favoriteNumber;
+
+    // This is a comment!
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    People[] public people;
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
     
-    uint256 public favoriteNumber=10; 
-    
-    
-function displayNumber() public view returns(uint256){
-    return favoriteNumber;
+    function retrieve() public view returns (uint256){
+        return favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
 }
-
-function performOperation(uint256 _favNumber) public pure returns(uint256){
-    return _favNumber*_favNumber; 
-}
-
-
-struct People {
-    uint256 age; 
-    string name; 
-}
-
-People[] public people; 
-mapping(string=>uint256) public nameToAge; 
-
-function addPerson(string memory _name, uint256 _age) public {
-    people.push(People(_age, _name)); 
-    nameToAge[_name] = _age; 
-}
-
-}
-
